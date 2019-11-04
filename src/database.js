c09927beb7c05bed06d63c11d1feedbc22cb3eb9
Rafael <r.brens@gbh.com.do>
2019-11-04 18:33:07 +0100

    Connect to DB

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+<<<<<<< HEAD
 class Database {
     constructor() {
         this.server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
@@ -19,6 +20,27 @@ class Database {
             .catch(() => {
                 console.error('Database connection error');
             });
+=======
+const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
+const database = 'memo'; // REPLACE WITH YOUR DB NAME
+
+class Database {
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        mongoose.connect(`mongodb://${server}/${database}`, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+            .then(() => {
+                console.log('Database connection successful')
+            })
+            .catch(err => {
+                console.error('Database connection error')
+            })
+>>>>>>> Connect to DB
     }
 }
 
