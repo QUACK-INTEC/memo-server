d475c6faae57744a0695bf2d14c8b0fdae5e988c
Rafael <r.brens@gbh.com.do>
2019-11-06 00:11:15 +0100

    Fix camel case

--- src/models/SubjectModel.js
+++ src/models/SubjectModel.js
@@ -10,7 +10,7 @@ const subjectModel = new Schema({
         required: true,
         unique: true,
     },
-    Code: {
+    code: {
         type: String,
         required: true,
         unique: true,
