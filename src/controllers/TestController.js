0b03743c876f688bca5c13d39aaca04618fc1388
Rafael <r.brens@gbh.com.do>
2019-11-06 00:03:37 +0100

    Fix linting issues

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,17 +1,4 @@
 class TestController {
-    constructor() {
-    }
-
-    service = {};
-
-    search(params) {
-        try {
-            const e = this.service.getSomeEntity();
-            return e;
-        } catch (e) {
-            return { someHttpErrorMessage: e.message };
-        }
-    }
 }
 
 module.exports = new TestController();
