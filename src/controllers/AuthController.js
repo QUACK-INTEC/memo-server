01fe20080397becff1ff171dae8adac75f354ccc
Rafael <r.brens@gbh.com.do>
2019-11-11 21:49:17 +0100

    Standarize errors

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const { AUTH_SECRET } = require('../config/config');
 const UserService = require('../services/UserService');
 const { serializeUser } = require('../utils/serializers');
+const HttpResponse = require('./responses/HttpResponse');
 
 const genToken = (id, email) => jwt.sign({ id, email }, AUTH_SECRET, {
     expiresIn: 604800, // 1 week
@@ -16,13 +17,11 @@ const register = async (req, res) => {
     } = req.body;
 
     if (!email || !password || !firstName || !lastName) {
-        res.status(400).json({ success: false, msg: 'Missing fields' }); // FIXME Error standarization
-        return;
+        return new HttpResponse({ res }).badRequest(null, 'Missing fields');
     }
 
     if (!validator.isEmail(email)) {
-        res.status(400).json({ success: false, msg: 'Invalid email address' }); // FIXME Error standarization
-        return;
+        return new HttpResponse({ res }).badRequest(null, 'Invalid email address');
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -36,10 +35,7 @@ const register = async (req, res) => {
     };
 
     const newUser = await UserService.create(userInfo);
-    res.json({
-        success: true,
-        user: serializeUser(newUser),
-    });
+    return new HttpResponse({ res }).ok({ user: serializeUser(newUser) }, 'Invalid email address');
 };
 
 const login = async (req, res) => {
