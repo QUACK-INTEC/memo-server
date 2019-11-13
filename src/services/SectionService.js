84b9e493069742d6f527f3fc06d19f398e88cb2b
Rafael <r.brens@gbh.com.do>
2019-11-13 19:08:50 +0100

    Add logic in section service

--- src/services/SectionService.js
+++ src/services/SectionService.js
@@ -5,12 +5,37 @@ const { SectionModel } = require('../models');
 const findMySections = async (userId) => {
     const id = new mongoose.Types.ObjectId(userId);
 
-    const a = await SectionModel
-        .find({ students: mongoose.Schema.ObjectId(id) }).lean().exec();
-    console.log(a);
-    return a;
+    const result = await SectionModel
+        .find({ students: id })
+        .populate('students', 'firstName lastName email points').lean().exec();
+    return result;
+};
+
+const findById = async (id) => {
+    const result = await SectionModel
+        .findById(id).lean().exec();
+    return result;
+};
+
+const findSectionsStudents = async (id) => {
+    const res = await SectionModel
+        .findById(id).populate('students', 'firstName lastName email points').lean().exec();
+    return res.students;
+};
+
+const findSectionsPosts = async (id) => {
+    const res = await SectionModel
+        .findById(id)
+        .populate(
+            'posts',
+            'title description startDate endDate type author',
+        ).lean().exec();
+    return res.posts;
 };
 
 module.exports = {
     findMySections,
+    findById,
+    findSectionsStudents,
+    findSectionsPosts,
 };
