f035bab6d4be557cc4a1bb9bfe77dabc7d173a36
Rafael <r.brens@gbh.com.do>
2019-11-13 19:08:07 +0100

    Add routes

--- src/routes/sections.js
+++ src/routes/sections.js
@@ -6,5 +6,8 @@ const SectionController = require('../controllers/SectionController');
 const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), asyncHandler(SectionController.getMySections));
+router.get('/:id', passport.authenticate('jwt', { session: false }), asyncHandler(SectionController.sectionDetails));
+router.get('/:id/students', passport.authenticate('jwt', { session: false }), asyncHandler(SectionController.sectionStudents));
+router.get('/:id/posts', passport.authenticate('jwt', { session: false }), asyncHandler(SectionController.sectionPosts));
 
 module.exports = router;
