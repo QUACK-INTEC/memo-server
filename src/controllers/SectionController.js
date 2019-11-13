d01647a387491441dca7a1dfaf77b7d4c7aa397f
Rafael <r.brens@gbh.com.do>
2019-11-13 19:08:37 +0100

    Add methods in section controller

--- src/controllers/SectionController.js
+++ src/controllers/SectionController.js
@@ -1,6 +1,6 @@
 
 const SectionService = require('../services/SectionService');
-const { serializeSection } = require('../utils/serializers');
+const { serializeSection, serializeSectionStudent, serializeSectionPost } = require('../utils/serializers');
 
 const getMySections = async (req, res) => {
     const { id } = req.user;
@@ -8,11 +8,40 @@ const getMySections = async (req, res) => {
     const sections = await SectionService.findMySections(id);
     res.json({
         success: true,
-        sections: sections.map((section) => serializeSection(section)),
+        data: sections.map((section) => serializeSection(section)),
     });
 };
 
+const sectionDetails = async (req, res) => {
+    const { id } = req.params;
+    const section = await SectionService.findById(id);
+    res.json({
+        success: true,
+        data: serializeSection(section),
+    });
+};
+
+const sectionStudents = async (req, res) => {
+    const { id } = req.params;
+    const students = await SectionService.findSectionsStudents(id);
+    res.json({
+        success: true,
+        data: students.map((s) => (serializeSectionStudent(s))),
+    });
+};
+
+const sectionPosts = async (req, res) => {
+    const { id } = req.params;
+    const posts = await SectionService.findSectionsPosts(id);
+    res.json({
+        success: true,
+        data: posts.map((p) => (serializeSectionPost(p))),
+    });
+};
 
 module.exports = {
     getMySections,
+    sectionDetails,
+    sectionStudents,
+    sectionPosts,
 };
