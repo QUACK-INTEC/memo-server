d375d3c8cdf2d867aa5110451696fc4e417ada16
Rafael <r.brens@gbh.com.do>
2020-02-04 12:00:11 +0100

    close connection

--- jobs/cleanUpAttachements.js
+++ jobs/cleanUpAttachements.js
@@ -1,10 +1,13 @@
+const mongoose = require('mongoose');
+
+require('../src/database');
+
 const aws = require('aws-sdk');
 const winston = require('winston');
 const { AWS_S3_BUCKET_NAME, AWS_S3_ENDPOINT } = require('../src/config/config');
 const { PostModel, AttachmentModel } = require('../src/models');
 const { S3_ATTACHMENTS_PREFIX } = require('../src/constants/aws');
 
-require('../src/database');
 
 const s3 = new aws.S3({
     endpoint: new aws.Endpoint(AWS_S3_ENDPOINT),
@@ -39,6 +42,7 @@ const cleanUpAttachements = async () => {
     } catch (error) {
         winston.error(error);
     }
+    await mongoose.connection.close();
 };
 
 module.exports = cleanUpAttachements;
